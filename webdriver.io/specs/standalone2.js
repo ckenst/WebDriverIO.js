faab20f6677af81e53a435f4233f37fd579f1513
Chris Kenst <ckenst@gmail.com>
2018-06-11 16:59:02 -0700

    simple working tests

---
+++ webdriver.io/specs/standalone2.js
@@ -0,0 +1,17 @@
+// You can run this sample by using node. Won't work using wdio
+
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'chrome'
+    }
+};
+
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function(title) {
+        console.log('Title was: ' + title);
+    })
+    .end();
