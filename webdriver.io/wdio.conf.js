1bb6760a3846df417412256c2eef1e0bc3c3d224
Chris Kenst <ckenst@gmail.com>
2018-06-18 16:57:03 -0700

    Updated

--- webdriver.io/wdio.conf.js
+++ webdriver.io/wdio.conf.js
@@ -8,8 +8,6 @@ exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    // user: 'bloomnation1',
-    // key: 'k3JzfsySJZ4BB7GzMjmy',
     //
     // ==================
     // Specify Test Files
