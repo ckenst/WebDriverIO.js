a774e2b42ebf1df05ef939fdd2bf3f3a7eb89ee7
Chris Kenst <ckenst@gmail.com>
2018-06-22 18:30:21 -0700

    added a wait

--- specs/kenst/about.spec.js
+++ specs/kenst/about.spec.js
@@ -3,26 +3,22 @@ var HomePage  = require('../../pageobjects/kenst.com.page');
 var assert    = require('assert');
 
 describe('About kenst.com', function() {
-    // Example using getTitle
     it('should have the right title', function() {
         HomePage.open();
+        HomePage.navResources.waitForExist();
         HomePage.navAbout.click();
         var title = browser.getTitle();
         assert.equal(title, 'About – Chris Kenst');
     });
-    // Example using getAttribute
     it('should have the right href', function() {
         browser.url('https://www.kenst.com/about');
         var attr = browser.getAttribute('=About', 'href')
         assert.equal(attr, 'https://www.kenst.com/about/')
     });
-    // Example using getTagName
     it('title should be an h1', function()  {
         AboutPage.open();
         var title = browser.getTagName('.entry-title')
         assert.equal(title, 'h1')
     });
-
-    // Example of a pending test
     it('should be a pending test');
 });
