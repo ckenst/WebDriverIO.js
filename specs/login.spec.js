4552660f3e7f19f582226e64839f72dafb13eca9
Chris Kenst <ckenst@gmail.com>
2018-07-05 12:29:17 -0700

    Using fixture data. Closes #2

--- specs/login.spec.js
+++ specs/login.spec.js
@@ -1,20 +1,21 @@
 var expect = require('chai').expect;
-var LoginPage = require('../pageobjects/login.page')
+var LoginPage = require('../pages/login.page');
+var LoginData = require('../fixtures/login');
 
 describe('login form', function() {
   it('should deny acces with the wrong creds', function(){
     LoginPage.open();
-    LoginPage.username.setValue('foo');
-    LoginPage.password.setValue('bar');
+    LoginPage.username.setValue(LoginData.invalid.username);
+    LoginPage.password.setValue(LoginData.invalid.password);
     LoginPage.submit(); // this should be a click event
 
-    expect(LoginPage.flash.getText()).to.contain('Your username is invalid!');
+    expect(LoginPage.flash.getText()).to.contain('Your username is invalid!\n×'); // clearly a typo
   });
 
   it('should allow access with correct creds', function() {
     LoginPage.open();
-    LoginPage.username.setValue('tomsmith');
-    LoginPage.password.setValue('SuperSecretPassword!');
+    LoginPage.username.setValue(LoginData.valid.username);
+    LoginPage.password.setValue(LoginData.valid.password);
     LoginPage.submit(); // this should be a click event
 
     expect(LoginPage.flash.getText()).to.contain('You logged into a secure area!');
