25e73fa00442e2e5f95ed8b39c07d02840abeb6d
Chris Kenst <ckenst@gmail.com>
2019-01-30 17:57:10 -0800

    Using geotest site and printing details of location

--- specs/standalone/standalone3.js
+++ specs/standalone/standalone3.js
@@ -5,7 +5,7 @@ const options = {
     desiredCapabilities: {
         browserName: 'chrome',
         proxy: {
-            proxyType: "manual",
+            proxyType: "MANUAL",
             httpProxy: "http://saltlakecity.wonderproxy.com:11000",
             socksUsername: "username",
             socksPassword: "password",
@@ -16,9 +16,17 @@ const options = {
 webdriverio
     .remote(options)
     .init()
-    .url('https://www.iplocation.net/')
+    .url('https://wondernetwork.com/geotest')
     .getTitle().then(function(title) {
         console.log('Title was: ' + title);
     })
-    .saveScreenshot('whereami.png')
+    .getText('#user-country').then(function(name) {
+        console.log('Country name is: ' + name);
+    })
+    .getText('#user-region').then(function(region) {
+        console.log('Region name is: ' + region);
+    })
+    .getText('#user-city').then(function(city) {
+        console.log('City name is: ' + city);
+    })
     .end();
